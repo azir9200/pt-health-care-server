48ca97675c32e3af5bbbcd38bf3323f297145433
Azir@9200 <aziruddin83@hotmail.com>
2025-11-03 09:31:35 +0000

    doctor schedule created

--- src/app/modules/schedule/schedule.controller.ts
+++ src/app/modules/schedule/schedule.controller.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 import catchAsync from "../../shared/catchAsync";
 import sendResponse from "../../shared/sendResponse";
 import { ScheduleService } from "./schedule.service";
+import { IJWTPayload } from "../../types/common";
+import pick from "../../helper/pick";
 
 const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
   const result = await ScheduleService.insertIntoDB(req.body);
@@ -14,6 +16,40 @@ const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+const schedulesForDoctor = catchAsync(
+  async (req: Request & { user?: IJWTPayload }, res: Response) => {
+    const options = pick(req.query, ["page", "limit", "sortBy", "sortOrder"]);
+    const filters = pick(req.query, ["startDateTime", "endDateTime"]);
+
+    const user = req.user;
+    const result = await ScheduleService.schedulesForDoctor(
+      user as IJWTPayload,
+      filters,
+      options
+    );
+
+    sendResponse(res, {
+      statusCode: 200,
+      success: true,
+      message: "Schedule fetched successfully!",
+
+      data: result,
+    });
+  }
+);
+const deleteScheduleFromDB = catchAsync(async (req: Request, res: Response) => {
+  const result = await ScheduleService.deleteScheduleFromDB(req.params.id);
+
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Schedule deleted successfully!",
+    data: result,
+  });
+});
+
 export const ScheduleController = {
   insertIntoDB,
+  schedulesForDoctor,
+  deleteScheduleFromDB,
 };
