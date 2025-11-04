906095b8d24fecf1561a0847c2f13aa83417e507
Azir@9200 <aziruddin83@hotmail.com>
2025-11-04 21:25:21 +0000

    created function for update doctor info

--- src/app/modules/specialties/specialties.routes.ts
+++ src/app/modules/specialties/specialties.routes.ts
@@ -1,13 +1,12 @@
-import express, { NextFunction, Request, Response } from 'express';
-import { SpecialtiesController } from './specialties.controller';
-import auth from '../../middlewares/auth';
-import { UserRole } from '@prisma/client';
-import { fileUploader } from '../../helper/fileUploader';
-import { SpecialtiesValidation } from './specialties.validation';
+import express, { NextFunction, Request, Response } from "express";
+import { SpecialtiesController } from "./specialties.controller";
+import auth from "../../middlewares/auth";
+import { UserRole } from "@prisma/client";
+import { fileUploader } from "../../helper/fileUploader";
+import { SpecialtiesValidation } from "./specialties.validation";
 
 
 const router = express.Router();
-
 
 // Task 1: Retrieve Specialties Data
 
@@ -16,21 +15,17 @@ const router = express.Router();
 - Implement an HTTP GET endpoint returning specialties in JSON format.
 - ENDPOINT: /specialties
 */
-router.get(
-    '/',
-    SpecialtiesController.getAllFromDB
-);
+router.get("/", SpecialtiesController.getAllFromDB);
 
 router.post(
-    '/',
-    fileUploader.upload.single('file'),
-    (req: Request, res: Response, next: NextFunction) => {
-        req.body = SpecialtiesValidation.create.parse(JSON.parse(req.body.data))
-        return SpecialtiesController.insertIntoDB(req, res, next)
-    }
+  "/",
+  auth(UserRole.ADMIN),
+  fileUploader.upload.single("file"),
+  (req: Request, res: Response, next: NextFunction) => {
+    req.body = SpecialtiesValidation.create.parse(JSON.parse(req.body.data));
+    return SpecialtiesController.insertIntoDB(req, res, next);
+  }
 );
-
-
 
 // Task 2: Delete Specialties Data by ID
 
@@ -42,9 +37,9 @@ router.post(
 */
 
 router.delete(
-    '/:id',
-    auth(UserRole.ADMIN, UserRole.ADMIN),
-    SpecialtiesController.deleteFromDB
+  "/:id",
+  auth(UserRole.ADMIN, UserRole.ADMIN),
+  SpecialtiesController.deleteFromDB
 );
 
 export const SpecialtiesRoutes = router;
